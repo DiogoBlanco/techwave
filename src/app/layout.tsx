1ed7c7b3a9ae1018659941b8b9cdebe4e9c7e020
DiogoBlanco <diogoblanco@outlook.com.br>
2023-11-06 10:39:26 -0300

    css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,12 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Techwave | Home',
   description: 'Techwave sistemas e soluções',
-};
-
-export const viewport: Metadata = {
-  viewport: 'width=device-width, initial-scale=1',
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import { Metadata } from 'next';
+
+export const viewport: Metadata = {
+  viewport: 'width=device-width, initial-scale=1',
+};
+
 export default function Home() {
-  return <main className="container"></main>;
+  return (
+    <main className="container">
+      <h1>Techwave - Home</h1>
+    </main>
+  );
 }
